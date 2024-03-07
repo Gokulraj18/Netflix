3ec73db929c814f725cf1c82dc7c67d75f9b10f9
gokulraj18 <rgokul527@gmail.com>
2024-03-07 20:38:44 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html/input.css'],
+  theme: {
+    extend: {
+      primary : '#243c5a', 
+      secondary : {
+        100: '#243c5a',
+      }
+    },
+  },
+  plugins: [],
+}
